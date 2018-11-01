75e25b88e5fa78eb211cc47ddeb9a3bb6516f820
papparazzo <pappi-@gmx.de>
2018-11-01 23:07:45 +0100

    throws exception on invalid symbol

--- symbol.cpp
+++ symbol.cpp
@@ -1,7 +1,33 @@
+/*
+ *  Project:    moba-svg
+ *
+ *  Copyright (C) 2016 Stefan Paproth <pappi-@gmx.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
 #include "symbol.h"
 
 #include <exception>
 #include <stdexcept>
+
+Symbol::Symbol(std::uint8_t symbol) : symbolFix{symbol}, symbolDyn{symbol} {
+    if(isSymbol() && !isValidSymbol()) {
+        throw std::invalid_argument("invalid symbol given");
+    }
+}
 
 Symbol::operator bool() const {
     return isSymbol();
--- symbol.h
+++ symbol.h
@@ -1,3 +1,23 @@
+/*
+ *  Project:    moba-svg
+ *
+ *  Copyright (C) 2016 Stefan Paproth <pappi-@gmx.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
 #pragma once
 
 #include <cstdint>
@@ -5,9 +25,8 @@
 
 class Symbol {
     public:
-        Symbol(std::uint8_t symbol) : symbolFix{symbol}, symbolDyn{symbol} {
-        }
-
+        Symbol(std::uint8_t symbol);
+        
         virtual ~Symbol() {
         }
 
