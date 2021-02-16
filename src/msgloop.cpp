41f3c22852fc1f071db3b5a5c9e572ceaf7a5caf
papparazzo <pappi-@gmx.de>
2021-02-16 21:10:01 +0100

    msgloop added

---
+++ src/msgloop.cpp
@@ -0,0 +1,67 @@
+/*
+ *  Project:    moba-blocksystem
+ *
+ *  Copyright (C) 2019 Stefan Paproth <pappi-@gmx.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
+#include <thread>
+#include <string>
+#include <moba-common/log.h>
+
+#include "msgloop.h"
+#include "moba/registry.h"
+#include "layoutparser.h"
+#include "svgdocument.h"
+
+MessageLoop::MessageLoop(EndpointPtr endpoint) : endpoint{endpoint}, closing{false} {
+}
+
+void MessageLoop::run() {
+    while(!closing) {
+        try {
+            Registry registry;
+            registry.registerHandler<GetLayout>([this](const GetLayout &d){parseLayout(d);});
+            registry.registerHandler<GetBlockingContacts>([this](const GetBlockingContacts &d){getFeedbackContactList(d);});
+
+            endpoint->connect();
+            endpoint->sendMsg(ControlGetContactListReq{});
+
+            while(true) {
+                registry.handleMsg(endpoint->waitForNewMsg());
+            }
+        } catch(const std::exception &e) {
+            LOG(moba::common::LogLevel::ERROR) << "exception occured! <" << e.what() << ">" << std::endl;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds{500});
+    }
+}
+
+void MessageLoop::getFeedbackContactList(const GetBlockingContacts &d) {
+    blockContacts = d.blockContacts;
+    endpoint->sendMsg(LayoutGetLayoutReadOnlyReq{});
+}
+
+void MessageLoop::parseLayout(const MessageLoop::GetLayout &d) {
+    LayoutParser parser;
+    LineVector lines = parser.parse(d.symbols);
+    SvgDocument svg{"/home/stefan/Documents/moba/quellcode/moba-display/src/www-data/img/test.svg", d.symbols->getHeight() + 2, d.symbols->getWidth() + 2};
+    svg.addLayout(lines);
+
+    for(auto &iter : *blockContacts) {
+        svg.addText(iter.first.x, iter.first.y, "b" + std::to_string(iter.second->id), "AA");
+    }
+}
---
+++ src/msgloop.h
@@ -0,0 +1,88 @@
+/*
+ *  Project:    moba-blocksystem
+ *
+ *  Copyright (C) 2019 Stefan Paproth <pappi-@gmx.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
+#pragma once
+
+#include <boost/noncopyable.hpp>
+
+#include "moba/layoutmessages.h"
+#include "moba/interfacemessages.h"
+#include "moba/controlmessages.h"
+#include "moba/endpoint.h"
+#include <memory>
+#include "common.h"
+
+
+using SymbolPtr = std::shared_ptr<Symbol>;
+using LayoutContainerPtr = std::shared_ptr<Container<SymbolPtr>>;
+
+using BlockContactPtr = std::shared_ptr<BlockContactData>;
+using ContactMapPtr = std::shared_ptr<std::map<Position, BlockContactPtr>>;
+
+class MessageLoop : private boost::noncopyable {
+
+    struct GetLayout : public LayoutMessage {
+        static constexpr std::uint32_t MESSAGE_ID = LAYOUT_GET_LAYOUT_RES;
+
+        GetLayout(const rapidjson::Document &d) {
+            symbols = std::make_shared<Container<SymbolPtr>>();
+            for(auto &iter : d["symbols"].GetArray()) {
+                symbols->addItem(
+                    {
+                        static_cast<std::size_t>(iter["xPos"].GetInt()),
+                        static_cast<std::size_t>(iter["yPos"].GetInt())
+                    },
+                    std::make_shared<Symbol>(iter["symbol"].GetInt())
+                );
+            }
+        }
+
+        LayoutContainerPtr symbols;
+    };
+
+    struct GetBlockingContacts : public ControlMessage {
+        static constexpr std::uint32_t MESSAGE_ID = CONTROL_GET_CONTACT_LIST_RES;
+
+        GetBlockingContacts(const rapidjson::Document &d) {
+            blockContacts = std::make_shared<std::map<Position, BlockContactPtr>>();
+
+            for(auto &iter : d.GetArray()) {
+                (*blockContacts)[{
+                    static_cast<std::size_t>(iter["xPos"].GetInt()),
+                    static_cast<std::size_t>(iter["yPos"].GetInt())
+                }] = std::make_shared<BlockContactData>(iter);
+            }
+        }
+
+        ContactMapPtr blockContacts;
+    };
+
+    EndpointPtr endpoint;
+    ContactMapPtr blockContacts;
+
+    void parseLayout(const GetLayout &d);
+    void getFeedbackContactList(const GetBlockingContacts &d);
+
+    bool closing;
+
+public:
+    MessageLoop(EndpointPtr endpoint);
+    void run();
+};
